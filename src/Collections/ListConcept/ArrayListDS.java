0bd9234c99cccfad3dd51fa8fddcb77aa03f7efb
unknown <mrunalmetkar@gmail.com>
2022-07-14 16:11:52 +0530

    Adding ArrayList demo

--- src/Collections/ListConcept/ArrayListDS.java
+++ src/Collections/ListConcept/ArrayListDS.java
@@ -1,6 +1,8 @@
 package Collections.ListConcept;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ArrayListDS {
 
@@ -9,18 +11,45 @@ public class ArrayListDS {
     //3. maintains insertion order
     //4. can be randomly accessed as it uses indexes internally
     //5. both generic nd nongeneric
+    //6. virtual defaullt capacity will be 10
     public static void main(String[] args) {
-        ArrayList arrayList=new ArrayList();  //nongeneric
+        ArrayList<String> arrayList = new ArrayList<String>();  //nongeneric
         arrayList.add("A");
-        arrayList.add(12);
+        arrayList.add("B");
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.println(arrayList.get(i));
         }
 
-        ArrayList<Integer> arrayList1=new ArrayList<Integer>(); //Generic
+        ArrayList<Integer> arrayList1 = new ArrayList<Integer>(); //Generic //virtual capacity done as 10
+
+        //Virtual capacity doesn't have any space in bytes/bits
+        //arr.size will give physical capacity
 
         //can also store a class object in arraylist
         //Arraylist<Employee> arr=new ArrayList<Employee>();
-        //arr.add(e1); arr.add(e2);    e1 and e2 are Employee class objects
+        //arr.add(e1); arr.add(e2);    e1 and e2 are Employee class
+
+
+        //ways to iterate arrayist
+        //1. typical for
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+
+        }
+
+        //2. JDK 8 - streams with lambda
+        arrayList.stream().forEach(s -> System.out.println(s));
+
+        //3. Iterator approach
+        Iterator<String> it = arrayList.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        ArrayList<Integer> list=new ArrayList<Integer>(Arrays.asList(1,2,3));
+    System.out.println(list);
     }
+
+
+
 }
