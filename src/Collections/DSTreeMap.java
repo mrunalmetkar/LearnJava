5659c70c4bf4050e4ff6fa240d8046a98b934ec9
unknown <mrunalmetkar@gmail.com>
2022-07-23 14:24:22 +0530

    Adding files

--- src/Collections/DSTreeMap.java
+++ src/Collections/DSTreeMap.java
@@ -4,6 +4,22 @@ import java.util.AbstractMap;
 import java.util.TreeMap;
 
 public class DSTreeMap {
-    TreeMap<Integer, String> map=new TreeMap<Integer, String>();
-    map.put(1,"ABC");
+    //TreeMap internally has red-black tree
+    //keeps the map in sorted order
+    public static void main(String[] args) {
+        TreeMap<Integer, String> map=new TreeMap<Integer, String>();
+        map.put(1,"ABC");
+        map.put(3,"PQR");
+        map.put(5,"PQR");
+        map.put(2,"PQR");
+        map.put(4,"PQR");
+
+        map.forEach((k,v)->System.out.println(k+v));
+
+        System.out.println(map.lastEntry());;
+        System.out.println(map.lastKey());
+
+        System.out.println(map.headMap(5).keySet());
+    }
+
 }
