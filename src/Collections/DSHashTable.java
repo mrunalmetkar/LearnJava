49a02e2b126b7f5544ca1c437ec7eaddbfce37f2
unknown <mrunalmetkar@gmail.com>
2022-07-23 15:23:19 +0530

    Adding files

---
+++ src/Collections/DSHashTable.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.Hashtable;
+
+public class DSHashTable {
+
+    //JDK 1.1
+    //synchronized
+    //legacy collection API
+    //doesn't allow any null keys and values
+    //initial capacity -11
+    //load factor=0.75
+    public static void main(String[] args) {
+
+        Hashtable<Integer,String> hashtable=new Hashtable<>();
+        hashtable.put(1,"asc");
+
+  }
+}
